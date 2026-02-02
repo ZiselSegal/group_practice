eed74d6a0298ed56e2447ce029839de076f4e658
ZiselSegal <zisel.segal@gmail.com>
2026-02-02 14:24:39 +0200

    added square area function and fixex comments to explain what each function does

--- advance_function.py
+++ advance_function.py
@@ -5,9 +5,15 @@ def calc_circle_area(radius):
         return 'error cant have negative values in geometric shapes'
     else:
         return pi * radius ** 2
-    
+
+ # calculates the area of a triangle get base and hight as parameters  
 def calc_triangle_area(hight,base):
     if hight < 0 or base < 0:
         return 'error cant have negative values in geometric shapes'
     return hight * base / 2
-    
+
+# calculates the area of a square get side1 and side2 as parameters
+def calc_square_area(side1,side2):
+    if side1 < 0 or side2 < 0:
+        return 'error cant have negative values in geometric shapes'
+    return side1 * side2
