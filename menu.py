74f52a6f6159449c506fa0d73280b936f0adb7fd
YE <yaakovde123@gmail.com>
2026-02-02 14:30:29 +0200

    added user menu

---
+++ menu.py
@@ -0,0 +1,122 @@
+from symple_function import multiply,divide,add,subtract,absolute_val,power
+from advance_function import calc_circle_area,calc_triangle_area,calc_square_area
+while True:
+    try:
+        func_choice = input("""please choose which of the following functions: 
+                                                        1. multiply function
+                                                        2. divide function
+                                                        3. add function
+                                                        4. subtraction function
+                                                        5. 2 variable power function
+                                                        6. 2 variable root function
+                                                        7. absolute value function
+                                                        8. area of triangle
+                                                        9. area of rectangle/square function
+                                                        10. are of circe function """)
+        func_choice = int(func_choice)
+        break
+    except ValueError:
+        print("please eneter a number ")
+
+match func_choice:
+    case 1:
+        while True:
+            try:
+                a = int(input("the first value"))
+                b = int(input("the second value"))
+                result = multiply(a,b)
+                print(f"{a}*{b}={result}")
+                break
+            except ValueError:
+                print("please eneter a number ")
+    case 2: 
+        while True:
+            try:
+                a = int(input("the first value"))
+                b = int(input("the second value"))
+                result = divide(a,b)
+                print(f"{a}/{b}={result}")
+                break
+            except ValueError:
+                print("please eneter a number ")
+    case 3:
+        while True:
+            try:
+                a = int(input("the first value"))
+                b = int(input("the second value"))
+                result = add(a,b)
+                print(f"{a}+{b}={result}")
+                break
+            except ValueError:
+                print("please eneter a number ")
+    case 4:
+        while True:
+            try:
+                a = int(input("the first value"))
+                b = int(input("the second value"))
+                result = subtract(a,b)
+                print(f"{a}-{b}={result}")
+                break
+            except ValueError:
+                print("please eneter a number ")
+    case 5:
+        while True:
+            try:
+                a = int(input("the base value"))
+                b = int(input("the power value"))
+                result = power(a,b)
+                print(f"{a}^{b}={result}")
+                break
+            except ValueError:
+                print("please eneter a number ")
+    case 6:
+        while True:
+            try:
+                a = int(input("the radicand value"))
+                b = int(input("the index value"))
+                result = root(a,b)
+                print(f"{a}root{b}={result}")
+                break
+            except ValueError:
+                print("please eneter a number ")
+    case 7: 
+        while True:
+            try:
+                a = int(input("the value"))
+                result = absolute_val(a,b)
+                print(f"|{a}|={result}")
+                break
+            except ValueError:
+                print("please eneter a number ")
+    case 8:
+        while True:
+            try:
+                a = int(input("the hight value"))
+                b = int(input("the base value"))
+                result = calc_triangle_area(a,b)
+                print(f"height:{a}, base:{b} ={result}")
+                break
+            except ValueError:
+                print("please eneter a number ")
+    case 9:
+         while True:
+            try:
+                a = int(input("the both sides value"))
+                b = int(input("the top and bottom value. Optional"))
+                result = calc_square_area(a,b=a)
+                print(f"height:{a}, base:{b} ={result}")
+                break
+            except ValueError:
+                print("please eneter a number ")
+    case 10:
+         while True:
+            try:
+                a = int(input("the hight value"))
+                b = int(input("the base value"))
+                result = calc_circle_area(a,b)
+                print(f"height:{a}, base:{b} ={result}")
+                break
+            except ValueError:
+                print("please eneter a number ")
+    case _:
+        print("sorry no such function")
