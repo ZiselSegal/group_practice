4d14256b3100443cc9425797cb2825128bfa052b
yaakovizel <z7685727@gmail.com>
2026-02-02 15:23:59 +0200

    added raise

--- symple_function.py
+++ symple_function.py
@@ -1,4 +1,4 @@
-
+from class_error import NegativeNumberError
 # subtracts num2 from num1
 def subtract(num1,num2):
     return num1-num2
@@ -9,8 +9,6 @@ def add(num1,num2):
 
 # function devides num1 by num2 and returns error message if num2 is 0
 def divide(num1,num2):
-    if num2 == 0:
-        return 'error cant divide by zero'
     return num1 / num2
 
 #returns the num1 in power of num
@@ -24,7 +22,7 @@ def absolute_val(num1):
 #returns num1 in root num2
 def root(num1,num2):
     if num1 < 0 :
-        return 'error you cannot calculate a negative number'
+        raise NegtiveNumberErroe
     else:
         return num1 ** (1/num2)
    
